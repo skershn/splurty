77ac90c679882a9faba9286164bfd7294d7e563c
Shanna Kershner <shanna.kershner@gmail.com>
2019-11-16 02:40:55 +0000

    added info abt pizza being awesome

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,16 @@
 class Quote < ApplicationRecord
   validates :saying, presence: true, length: { maximum: 280, minimum: 3 }
   validates :author, presence: true, length: { maximum: 100, minimum: 3 }
+
+  def unique_tag
+    abbr = []
+    self.author.split(" ").each do |sub_string|
+      abbr << sub_string[0]
+    end
+    abbr.join + '#' + self.id.to_s
+  end
 end
+
+
+
+
